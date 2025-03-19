6a5c478fa2437c518c20b14894a2ea149b19e969
남기범 <gogot1103@naver.com>
2025-03-19 22:57:07 +0900

    fix: add initial model

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,4 +1,6 @@
-from sqlmodel import SQLModel, create_engine
+from backend.app.models import User
+from sqlalchemy import select
+from sqlmodel import Session, SQLModel, create_engine
 
 from app.core.config import settings
 
@@ -10,16 +12,14 @@ engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 # for more details: https://github.com/fastapi/full-stack-fastapi-template/issues/28
 
 
-def init_db() -> None:
-    # Tables should be created with Alembic migrations
-    # But if you don't want to use migrations, create
-    # the tables un-commenting the next lines
-    # from sqlmodel import SQLModel
+def init_db(session: Session) -> None:
+    session.add()
 
     # This works because the models are already imported and registered from app.models
     SQLModel.metadata.create_all(engine)
 
-    # user = session.exec(select(User).where(User.email == settings.FIRST_SUPERUSER)).first()
+    session.exec(select(User).where(User.email == settings.FIRST_SUPERUSER)).first()
+
     # if not user:
     #     user_in = UserCreate(
     #         email=settings.FIRST_SUPERUSER,
