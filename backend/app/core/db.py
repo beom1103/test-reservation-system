14c67093694868808a4029b91102b18bae03331d
남기범 <gogot1103@naver.com>
2025-03-19 22:58:31 +0900

    fix: db py

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -18,7 +18,7 @@ def init_db(session: Session) -> None:
     # This works because the models are already imported and registered from app.models
     SQLModel.metadata.create_all(engine)
 
-    session.exec(select(User).where(User.email == settings.FIRST_SUPERUSER)).first()
+    session.exec(select(User).where(User.email == "FIRST_SUPERUSER")).first()
 
     # if not user:
     #     user_in = UserCreate(
